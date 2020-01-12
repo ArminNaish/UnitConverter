cdc8366c30734b854cf8798cc3751d8b0c701fa7
Armin Naish <armin.naish@outlook.com>
2020-01-12 20:12:08 +0100

    initial checkin

---
+++ unit_converter/tests/test_unit_converter.py
@@ -0,0 +1,24 @@
+import pytest
+from ..unit_converter import convert
+
+@pytest.mark.parametrize("value,from_unit, to_unit, expected", [
+    (5, 'gram', 'kilogram', (5e-3, 'kilogram')),
+    (5, 'milligram', 'gram', (5e-3, 'gram')),
+    (5,'gram', 'milligram', (5e3, 'milligram')),
+    (5, 'kilogram', 'gram', (5e3, 'gram')),
+    (5, 'gigagram', 'megagram', (5e3, 'megagram')),
+    (5, 'megagram', 'gigagram', (5e-3, 'gigagram')),
+])
+def test_factor_conversion(value, from_unit, to_unit, expected):
+    actual = convert(value, from_unit, to_unit)
+    assert actual == expected
+
+
+@pytest.mark.parametrize("value,from_unit, to_unit, expected", [
+    (68, 'degree fahrenheit', 'degree celsius', (20, 'degree celsius')),
+    (20, 'degree celsius', 'degree fahrenheit', (68, 'degree fahrenheit'))
+])
+def test_offset_conversion(value, from_unit, to_unit, expected):
+    value, unit = convert(value, from_unit, to_unit)
+    assert round(value,0) == expected[0]
+    assert unit == expected[1]
---
+++ unit_converter/unit_converter.py
@@ -0,0 +1,73 @@
+unit_conversions = []
+# 1 step conversions
+unit_conversions.append({'from': 'milligram', 'to': 'gram','from_offset': 0,  'multiplicand': 1, 'denominator': 10**3, 'to_offset': 0})
+unit_conversions.append({'from': 'gram', 'to': 'kilogram','from_offset': 0,  'multiplicand': 1, 'denominator': 10**3, 'to_offset': 0})
+unit_conversions.append({'from': 'gram', 'to': 'megagram', 'from_offset': 0, 'multiplicand': 1, 'denominator': 10**6, 'to_offset': 0})
+unit_conversions.append({'from': 'gram', 'to': 'gigagram','from_offset': 0,  'multiplicand': 1, 'denominator': 10**9, 'to_offset': 0})
+
+unit_conversions.append({'from': 'gram', 'to': 'milligram','from_offset': 0,  'multiplicand': 10**3, 'denominator': 1, 'to_offset': 0})
+unit_conversions.append({'from': 'kilogram', 'to': 'gram', 'from_offset': 0, 'multiplicand': 10**3, 'denominator': 1, 'to_offset': 0})
+unit_conversions.append({'from': 'megagram', 'to': 'gram','from_offset': 0,  'multiplicand': 10**6, 'denominator': 1, 'to_offset': 0})
+unit_conversions.append({'from': 'gigagram', 'to': 'gram','from_offset': 0,  'multiplicand': 10**9, 'denominator': 1, 'to_offset': 0})
+
+unit_conversions.append({'from': 'degree fahrenheit', 'to': 'kelvin', 'from_offset': 459.67, 'multiplicand': 1, 'denominator': 1.8, 'to_offset': 0})
+unit_conversions.append({'from': 'kelvin', 'to': 'degree celsius', 'from_offset': 0, 'multiplicand': 1, 'denominator': 1, 'to_offset': -273.15})
+
+unit_conversions.append({'from': 'kelvin', 'to': 'degree fahrenheit', 'from_offset': 0, 'multiplicand': 1.8, 'denominator': 1, 'to_offset': -459.67})
+unit_conversions.append({'from': 'degree celsius', 'to': 'kelvin', 'from_offset': 273.15, 'multiplicand': 1, 'denominator': 1, 'to_offset': 0})
+
+
+units = []
+units.append({'name': 'gram', 'symbol': 'g', 'quantity': 'mass', 'type': 'base', 'base_unit': 'gram'})
+units.append({'name': 'kilogram', 'symbol': 'kg', 'quantity': 'mass', 'type': 'derived', 'base_unit': 'gram'})
+units.append({'name': 'milligram', 'symbol': 'mg', 'quantity': 'mass', 'type': 'derived', 'base_unit': 'gram'})
+units.append({'name': 'megagram', 'symbol': 'Mg', 'quantity': 'mass', 'type': 'derived', 'base_unit': 'gram'})
+units.append({'name': 'gigagram', 'symbol': 'Gg', 'quantity': 'mass', 'type': 'derived', 'base_unit': 'gram'})
+units.append({'name': 'nanogram', 'symbol': 'Ng', 'quantity': 'mass', 'type': 'derived', 'base_unit': 'gram'})
+
+units.append({'name': 'kelvin', 'symbol': 'K', 'quantity': 'temperature', 'type': 'base', 'base_unit': 'kelvin'})
+units.append({'name': 'degree fahrenheit', 'symbol': 'F', 'quantity': 'temperature', 'type': 'derived', 'base_unit': 'kelvin'})
+units.append({'name': 'degree celsius', 'symbol': 'C', 'quantity': 'temperature', 'type': 'derived', 'base_unit': 'kelvin'})
+
+
+
+
+def convert(value, from_unit, to_unit):
+    # todo: check if unit is derived
+    # derived to base -> one step
+    # base to derived -> one step
+    # derived to derived -> two steps
+
+    # todo: add rounding param
+
+    #if from_unit == to_unit:
+    #    return value, from_unit
+
+    [unit1] = [u for u in units if u['name'] == from_unit]
+    [unit2] = [u for u in units if u['name'] == to_unit]
+
+    if unit1['base_unit'] != unit2['base_unit']:
+        raise Exception('Units are not convertable')
+
+    
+
+    # one step conversion
+    uc = first_or_default([uc for uc in unit_conversions if uc['from'] == from_unit and uc['to'] == to_unit])
+    if uc:
+        result = ((value + uc['from_offset']) * uc['multiplicand'] / uc['denominator']) + uc['to_offset']
+        return (result, uc['to'])
+    
+    # todo: if one unit is base there must be a conversion entry...
+    
+    # no conversion exists -> try two step conversion
+    [base_unit] = [u['base_unit'] for u in units if u['name'] == to_unit]
+
+    # todo: only supports two levels --> fix
+    x1 = convert(value, from_unit, base_unit)
+    return convert(*x1, to_unit)
+
+
+def first_or_default(list, default=None):
+    if list is None: return default
+    if not list: return default
+    return list[0]
